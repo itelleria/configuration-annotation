cb6dd83269d22b56db860b237eaa50d8ce1455ce
Iñigo Telleria <inigo@arima.eu>
2018-08-06 15:34:52 +0200

    Add another test

--- src/test/java/eu/arima/configurationannotation/NotAnnotatedConfigurationTest.java
+++ src/test/java/eu/arima/configurationannotation/NotAnnotatedConfigurationTest.java
@@ -3,6 +3,7 @@ package eu.arima.configurationannotation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,6 +18,15 @@ public class NotAnnotatedConfigurationTest {
     @Test
     public void load() {
         assertNotNull(this.demoService);
+    }
+
+    @Test
+    public void loadProgramatically() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(NotAnnotatedConfiguration.class);
+
+        DemoService service = applicationContext.getBean(DemoService.class);
+
+        assertNotNull(service);
     }
 
 }
